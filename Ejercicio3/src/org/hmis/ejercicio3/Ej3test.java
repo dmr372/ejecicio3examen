e68e2bb2abadf2dfbbc78228c66bd5ac16def3e2
Daniel Martinez <daniporre@hotmail.es>
2020-06-04 19:41:07 +0200

    commit

---
+++ Ejercicio3/src/org/hmis/ejercicio3/Ej3test.java
@@ -0,0 +1,19 @@
+package org.hmis.ejercicio3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class Ej3test {
+
+ @ParameterizedTest
+ @CsvSource({"0", "1", "2", "3", "4", "5", "6", "7", "8"})
+ 
+ void notaTest(int dia) {
+  Ejercicio3 ej = new Ejercicio3();
+  ej.diaSemana(dia);
+ }
+
+}
